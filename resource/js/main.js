f25fbd10fe42207eaee0e058205af6b696a3662d
SifatKhn <sifathossin8@gmail.com>
2022-02-10 01:28:02 +0600

    first commit

---
+++ resource/js/main.js
@@ -0,0 +1,11 @@
+//HUMBURGER MENU
+function openMenu(){
+    document.getElementById('ham-menu-container').style.width = "100%";
+}
+function closeMenu(){
+    document.getElementById('ham-menu-container').style.width = "0%";
+}
+// MIX IT UP
+var mixer = mixitup('.work-grid')
+// WOW
+new WOW().init();
